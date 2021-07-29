e4c256b1177fa24808782a5c7e9dd4622ff0343a
Daryl.Xu <xuziqiang@zyheal.com>
2021-07-30 00:08:25 +0800

    chore: use single quote

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dev_tools",
-    version="0.0.1",  # 本版本可以不进行更新
-    author="Daryl.Xu",
-    author_email="xuziqiang@zyheal.com",
-    description="General dev_tools written in Python",
+    name='dev_tools',
+    version='0.0.1',  # 本版本可以不进行更新
+    author='Daryl.Xu',
+    author_email='xuziqiang@zyheal.com',
+    description='General dev_tools written in Python',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/zyheal/dev-tools",
-    license="MIT",
+    long_description_content_type='text/markdown',
+    url='https://github.com/passer-team/dev-tools',
+    project_urls={
+        'HomePage': 'https://github.com/passer-team/dev-tools',
+        'Bug Report': 'https://github.com/passer-team/dev-tools/issues/new'
+    },
+    license='MIT',
     platforms=['Linux', 'Windows', 'MacOs'],
     packages=setuptools.find_packages(),  # 默认在.目录寻找模块（需要有__init__.py）
     include_package_data=True,
@@ -25,7 +29,8 @@ setuptools.setup(
             ]
     },
     classifiers=(
-        "Programming Language :: Python :: 3.8",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ),
 )
