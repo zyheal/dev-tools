e925bf9b01f10dea9693f2f85a81ce06e69f3b74
许自强 <xuziqiang@zyheal.com>
2024-03-01 17:09:24 +0800

    feat: 新增数字filter，根目录重定向，从pipenv切换到poetry

--- dev_tools/template_server.py
+++ dev_tools/template_server.py
@@ -1,10 +1,13 @@
 #! /bin/env python3
 import argparse
 import json
+import math
 import os
+from typing import Union
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 from jinja2 import Template
+import jinja2
 
 
 def _parse_args():
@@ -15,6 +18,28 @@ def _parse_args():
     parser.add_argument(
         'parameter', help='参数文件路径, eg: test-data/parameter.json')
     return parser.parse_args()
+
+
+def format_number(value: [int, float, math.nan, str],  # type: ignore
+                    digits:int = 0, 
+                    placeholder:any = '--'
+                    ) -> any:
+    """
+    格式化数字，传入字符串时尝试转为浮点数。当value为 None或math.NaN时返回default，传入其他无法转为数字的值
+    时则给出提示字符串。
+    Args:
+        value: 传入值
+        digits: 保留位数
+        placeholder: 用于替换 None 或 math.NaN 的值
+    """
+    if value is None or math.isnan(value):
+        return placeholder
+    try:
+        value = float(value)
+        return f'{value:.{digits}f}'  
+    except ValueError:
+        return 'Invalid number:' + str(value)
+
 
 def _main():
     args = _parse_args()
@@ -28,12 +53,18 @@ def _main():
     @app.route('/template.html')
     def serve_template():
         parameter = json.load(open(parameter_path, 'rb'))
-        with open(template_path, 'r', encoding='utf-8') as template_src:
-            template = Template(template_src.read())
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path))
+        env.filters['format_number'] = format_number
+        with open(template_path, 'r', encoding='utf-8') as f:
+            template = env.from_string(f.read())
         print("Rendering parameters got: ", parameter)
         return template.render(data=parameter)
+    
+    @app.route('/')
+    def root():
+        return redirect('template.html', code=302)
 
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
 if __name__ == '__main__':
